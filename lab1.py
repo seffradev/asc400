840eabc07b7f295532f33552dd58f9eb62122ead
Hampus Avekvist <ha@leissner.se>
2025-01-27 08:20:31 +0100

    chore: Create lab1 project file

---
+++ lab1.py
@@ -0,0 +1,32 @@
+"""
+Question 1:
+What makes an asymmetric scheme like RSA slower than its
+symmetric alternatives such as AES? Explain. Also, what
+information in the RSA key generator, encryption and
+decryption algorithms an attacker may have access to?
+
+Answer 1:
+
+Question 2:
+How many numbers from 1 to n should be tested before
+deciding if n is prime or not? Why?
+
+Answer 2:
+
+Question 3:
+Alice wants to send m=15 to Bob. She gets Bob's public
+key pk=(19,77) by visiting a public repostory. If she
+decides to use RSA for encryption, what would the
+resulting cipher text be? (Show your calculation)
+
+Answer 3:
+
+"""
+
+
+def main():
+    print("Hello, world!")
+
+
+if __name__ == "__main__":
+    main()
