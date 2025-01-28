5aae32ddd819c2a7af8e243a85931a95544e55d8
ali <aloosh.mma15@gmail.com>
2025-01-28 15:03:18 +0100

    task_2

--- test_lab1.py
+++ test_lab1.py
@@ -27,3 +27,37 @@ def is_prime(n):
         if n % i == 0:
             return False
     return True
+
+
+
+# Task 2
+def task_2(a,b):
+    kvot= []
+    rester= [a,b]
+    new_a= a
+    new_b= b
+
+    while rester[-1] != 0:
+        kvot.append(new_b//new_a)
+        rester.append(new_b % new_a)
+        new_b = new_a
+        new_a= rester[-1]
+
+    if(rester[-2] != 1):
+        return f"{a} och {b} är inte relativt prima"
+
+
+
+    xList = [0]
+    yList = [1]
+
+    for item in reversed(kvot):
+        x = yList[-1] - item * xList[-1]
+        y = xList[-1]
+        xList.append(x)
+        yList.append(y)
+
+    return f"modulära inversen av {a} mod {b} är {xList[-1] % b}"
+
+
+
