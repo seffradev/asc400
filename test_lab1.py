762db67b75187cdd8db14c6a6895504a15fd5501
Andreas Berger <aberger1981@outlook.com>
2025-01-28 16:47:21 +0100

    feat: move Task 1 to lab1

--- test_lab1.py
+++ test_lab1.py
@@ -12,24 +12,6 @@ def test_func():
     assert func(2, 3) == 5
 
 
-# Task 1:
-def is_prime(n):
-    if n <= 1:  # Numbers less than or equal to 1 are not prime.
-        return False
-    if n == 2:  # 2 is a prime number.
-        return True
-    if n % 2 == 0:  # Eliminate even numbers greater than 2
-        return False
-
-    # Check divisors from 3 to sqrt(n) (inclusive)
-    limit = int(n**0.5) + 1 # Calculate the integer sqrt(n)
-    for i in range(3, limit, 2):    # Only test odd numbers
-        if n % i == 0:
-            return False
-    return True
-
-
-
 # Task 2
 def modular_inverse(a,b):
     kvot= []
