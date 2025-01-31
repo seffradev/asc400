6c050ffc5adc1de1a772b53559b8856edc5cc0a9
Freddeko <fredrik_mik@live.se>
2025-01-31 07:47:20 +0100

    Moved Task 2 to lab1.py

--- test_lab1.py
+++ test_lab1.py
@@ -25,34 +25,3 @@ def test_is_prime():
     assert not is_prime(-1)
     assert not is_prime(-4)
     assert not is_prime(-17)
-
-
-# Task 2
-def modular_inverse(a,b):
-    kvot= []
-    rester= [a,b]
-    new_a= a
-    new_b= b
-
-    while rester[-1] != 0:
-        kvot.append(new_b//new_a)
-        rester.append(new_b % new_a)
-        new_b = new_a
-        new_a= rester[-1]
-
-    if(rester[-2] != 1):
-        raise ValueError(f"{a} och {b} är inte relativt prima")
-
-
-
-    xList = [0]
-    yList = [1]
-    for item in reversed(kvot):
-        x = yList[-1] - item * xList[-1]
-        y = xList[-1]
-        xList.append(x)
-        yList.append(y)
-
-    return xList[-1] % b
-
-
